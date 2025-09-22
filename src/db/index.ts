b684c89dd77b444d717449401bfaea9c9ad04890
Sathiya Moorthi <sathiyamoorthi.c85085@gmail.com>
2025-09-22 07:18:03 +0000

    No config path provided, using default 'drizzle.config.ts' Reading confi

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,10 @@
+import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
 
-export const db = drizzle(process.env.'postgresql://neondb_owner:npg_doHhpA4CqS9z@ep-nameless-river-adxz741w-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
+const sql = neon(process.env.DATABASE_URL);
+export const db = drizzle(sql);
