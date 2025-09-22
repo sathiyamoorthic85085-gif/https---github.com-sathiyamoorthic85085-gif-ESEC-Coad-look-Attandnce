a9f725690fa8daa725f6d94b058d72eb7ec7e1ff
Sathiya Moorthi <sathiyamoorthi.c85085@gmail.com>
2025-09-22 07:16:56 +0000

    No config path provided, using default 'drizzle.config.json' /home/user/

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,3 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 
-export const db = drizzle(process.env.DATABASE_URL);
+export const db = drizzle(process.env.'postgresql://neondb_owner:npg_doHhpA4CqS9z@ep-nameless-river-adxz741w-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
